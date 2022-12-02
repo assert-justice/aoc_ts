31bd6db0f5b9945a18fd400d9de7cda66cc56192
Riley Miller <waskerdu@gmail.com>
2022-12-01 21:33:51 -0800

    day 2!

--- 2022/d2-pt1.ts
+++ 2022/d2-pt1.ts
@@ -1,7 +1,32 @@
 import * as fs from 'fs';
+import {splitLines} from '../common';
+
+function score(line: string){
+    let [them, you] = line.split(' ');
+    const lookup = new Map([['A','X'],['B','Y'],['C','Z'],]);
+    them = lookup.get(them) ?? '';
+    const beats = new Map([['X','Z'],['Y','X'],['Z','Y'],]);
+    const pointLookup = new Map([['X',1],['Y',2],['Z',3]]);
+    let points = pointLookup.get(you) ?? 0;
+    if(beats.get(them) === you){}
+    else if(them === you){
+        points += 3;
+    }
+    else{
+        points += 6;
+    }
+    return points;
+}
 
 function main(text: string): void{
-    // Solution here.
+    const lines = splitLines(text);
+    let points = 0;
+    for (const line of lines) {
+        const add = score(line);
+        points+=add;
+    }
+    console.log(points);
+    
 }
 
 main(fs.readFileSync('./2022/d2-i.txt', {encoding: 'utf-8'}));
--- 2022/d2-pt2.ts
+++ 2022/d2-pt2.ts
@@ -1,7 +1,48 @@
 import * as fs from 'fs';
+import {splitLines} from '../common';
+
+function score(line: string){
+    let [them, you] = line.split(' ');
+    const lookup = new Map([['A','X'],['B','Y'],['C','Z'],]);
+    them = lookup.get(them) ?? '';
+    const beats = new Map([['X','Z'],['Y','X'],['Z','Y'],]);
+    const pointLookup = new Map([['X',1],['Y',2],['Z',3]]);
+    const outcomeLookup = new Map([['X',0],['Y',3],['Z',6]]);
+
+    let points = outcomeLookup.get(you) ?? 0;
+    if(points === 0){
+        you = beats.get(them) ?? '';
+    }
+    else if(points === 3){
+        you = them;
+    }
+    else{
+        // you = ([...beats.entries()].find(([t,y])=>y === them) ?? [0])[0];
+        
+        for (const [t,y] of beats.entries()) {
+            //console.log(t,y);
+            if(y === them){
+                you = t;
+                break;
+            }
+        }
+    }
+    points += pointLookup.get(you) ?? 0;
+    // console.log(points);
+    
+    return points;
+}
 
 function main(text: string): void{
-    // Solution here.
+    const lines = splitLines(text);
+    let points = 0;
+    for (const line of lines) {
+        const add = score(line);
+        // console.log(add);
+        points+=add;
+    }
+    console.log(points);
+    
 }
 
 main(fs.readFileSync('./2022/d2-i.txt', {encoding: 'utf-8'}));
