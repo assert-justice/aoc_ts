8f16385babb76d6b533ea715d041df9db9d315d6
riley <rileywestonmiller@outlook.com>
2022-12-11 22:31:57 -0800

    whoops

--- common.ts
+++ common.ts
@@ -12,6 +12,29 @@ export function tally<T>(data: T[]): Map<T,number>{
     return res;
 }
 
+export class Grid<T> {
+    data: T[];
+    width: number;
+    height: number;
+    constructor(width: number, height: number, startValue: T){
+        this.data = new Array<T>(width * height).fill(startValue).map(_=>startValue);
+        this.width = width; this.height = height;
+
+    }
+    getIdx(x: number, y: number): number{
+        return y*this.width+x;
+    }
+    get(x: number, y: number): T{
+        return this.data[this.getIdx(x,y)];
+    }
+    set(x: number, y: number, val: T): void{
+        this.data[this.getIdx(x,y)] = val;
+    }
+    onGrid(x: number, y: number):boolean{
+        return x >= 0 && x < this.width && y >= 0 && y < this.height;
+    }
+}
+
 // export class DefaultMap<T,R> extends Map{
 //     defaultValue: R;
 //     public constructor(defaultValue: R){
